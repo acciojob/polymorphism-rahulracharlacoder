df1f5d626dbbe096e28fab0b127281f870fb51cc
Rahul <rahulracharla369@gmail.com>
2024-11-28 00:32:32 +0530

    polymorphism assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(4,5);
+        p.product(4,5,6);
+        p.product(4.5, 5.6);
+    }
 
 }
